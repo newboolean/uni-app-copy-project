3a462a20af2f14e8eeec1d33a08568e4c6addf8d
newboolean <sunjiyan1228@163.com>
2024-01-13 15:23:49 +0800

    feat:引入uni-ui组件库

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -2,22 +2,27 @@
  * @Author: newboolean sunjiyan1228@163.com
  * @Date: 2024-01-13 13:32:59
  * @LastEditors: newboolean sunjiyan1228@163.com
- * @LastEditTime: 2024-01-13 13:52:54
+ * @LastEditTime: 2024-01-13 15:19:46
  * @FilePath: \my-vue3-project\src\pages\index\index.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
 <template>
   <view class="content">
-    <image class="logo" src="/static/logo.png"></image>
+    <image class="logo" src="./static/logo.png"></image>
     <view class="text-area">
       <text class="title">{{ title }}</text>
     </view>
+    <uni-card title="基础卡片" extra="额外信息">
+      <text
+        >这是一个基础卡片示例，此示例展示了一个标题加标题额外信息的标准卡片。</text
+      >
+    </uni-card>
   </view>
 </template>
 
-<script setup >
-  import { ref } from 'vue'
-  const title = ref('master')
+<script setup>
+import { ref } from "vue";
+const title = ref("master");
 </script>
 
 <style>
---
+++ src/pages/my/my.vue
@@ -0,0 +1,49 @@
+<!--
+ * @Author: newboolean sunjiyan1228@163.com
+ * @Date: 2024-01-13 13:32:59
+ * @LastEditors: newboolean sunjiyan1228@163.com
+ * @LastEditTime: 2024-01-13 15:05:07
+ * @FilePath: \my-vue3-project\src\pages\index\index.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
+<template>
+    <view class="content">
+      <view class="text-area">
+        <text class="title">{{ title }}</text>
+      </view>
+    </view>
+  </template>
+  
+  <script setup >
+    import { ref } from 'vue'
+    const title = ref('my')
+  </script>
+  
+  <style>
+  .content {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+  
+  .logo {
+    height: 200rpx;
+    width: 200rpx;
+    margin-top: 200rpx;
+    margin-left: auto;
+    margin-right: auto;
+    margin-bottom: 50rpx;
+  }
+  
+  .text-area {
+    display: flex;
+    justify-content: center;
+  }
+  
+  .title {
+    font-size: 36rpx;
+    color: #8f8f94;
+  }
+  </style>
+  
