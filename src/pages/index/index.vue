e22ca3d1a53824ccc7ca7da0a28783048b7e5708
newboolean <sunjiyan1228@163.com>
2024-01-14 12:13:55 +0800

    feat:首页分类业务

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -2,7 +2,7 @@
  * @Author: newboolean sunjiyan1228@163.com
  * @Date: 2024-01-13 13:32:59
  * @LastEditors: newboolean sunjiyan1228@163.com
- * @LastEditTime: 2024-01-13 23:26:46
+ * @LastEditTime: 2024-01-14 12:11:39
  * @FilePath: \my-vue3-project\src\pages\index\index.vue
  * @Description: 首页
 -->
@@ -10,26 +10,49 @@
   <view class="content">
     <CustomNavbar></CustomNavbar>
     <XtxSwiper :list="bannerList"></XtxSwiper>
+    <CategoryPanel :list="categoryList"></CategoryPanel>
   </view>
 </template>
 
 <script setup>
 import { ref } from "vue";
-import { onLoad } from '@dcloudio/uni-app'
-import { getHomeBannerAPI } from "@/services/index.js";
+import { onLoad } from "@dcloudio/uni-app";
+import { getHomeBannerAPI, getHomeCategoryAPI } from "@/services/index.js";
 import CustomNavbar from "./components/CustomNavbar.vue";
+import CategoryPanel from "./components/CategoryPanel.vue";
 // 获取轮播图数据
 const bannerList = ref([]);
 const getHomeBannerData = async () => {
   const res = await getHomeBannerAPI();
   bannerList.value = res.result;
-  console.log('====================================');
-  console.log(bannerList.value);
-  console.log('====================================');
+};
+// 获取分类分区组件
+const categoryList = ref([]);
+const getDategoryList = async () => {
+  const res = await getHomeCategoryAPI();
+  categoryList.value = res.result;
+  console.log(categoryList.value, "categoryList.value");
 };
 onLoad(() => {
-  getHomeBannerData()
-})
+  Promise.all([getHomeBannerData(), getDategoryList()]);
+});
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+page {
+  background-color: #f7f7f7;
+  height: 100%;
+  overflow: hidden;
+}
+
+.viewport {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+}
+
+.scroll-view {
+  flex: 1;
+  overflow: hidden;
+}
+</style>
--- src/services/index.js
+++ src/services/index.js
@@ -1,5 +1,14 @@
+/*
+ * @Author: newboolean sunjiyan1228@163.com
+ * @Date: 2024-01-13 23:13:12
+ * @LastEditors: newboolean sunjiyan1228@163.com
+ * @LastEditTime: 2024-01-14 12:08:10
+ * @FilePath: \my-vue3-project\src\services\index.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import { http } from "../utils/http";
 
+// 获取轮播图
 export const getHomeBannerAPI = (distributionSite = 1) => {
   return http({
     method: "GET",
@@ -8,4 +17,11 @@ export const getHomeBannerAPI = (distributionSite = 1) => {
       distributionSite,
     },
   });
+};
+// 获取分类分区接口
+export const getHomeCategoryAPI = () => {
+  return http({
+    method: 'GET',
+    url: '/home/category/mutli',
+  })
 };
