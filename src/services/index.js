e31f5245d47f5aa593c11cbd6b97fe15fac8fe83
newboolean <sunjiyan1228@163.com>
2024-01-18 23:06:02 +0800

    feat:商品详情

--- src/services/index.js
+++ src/services/index.js
@@ -2,7 +2,7 @@
  * @Author: newboolean sunjiyan1228@163.com
  * @Date: 2024-01-13 23:13:12
  * @LastEditors: newboolean sunjiyan1228@163.com
- * @LastEditTime: 2024-01-18 19:55:12
+ * @LastEditTime: 2024-01-18 22:43:18
  * @FilePath: \my-vue3-project\src\services\index.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -64,3 +64,15 @@ export const getHotRecommendAPI = (url, data) => {
     url: '/category/top',
   })
 }
+/**
+ * 商品详情
+ * @param id 商品id
+ */
+ export const getGoodsByIdAPI = (id) => {
+  return http({
+    method: 'GET',
+    url: '/goods',
+    data: { id },
+  })
+}
+
