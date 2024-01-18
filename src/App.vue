7bc8dbcbef9c0bfd3b078b34cb08628c042c17ad
newboolean <sunjiyan1228@163.com>
2024-01-18 23:06:19 +0800

    feat:全局样式+图标

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,60 @@
-<script>
-export default {
-  onLaunch: function () {
-    console.log('App Launch')
-  },
-  onShow: function () {
-    console.log('App Show')
-  },
-  onHide: function () {
-    console.log('App Hide')
-  },
-}
+<!--
+ * @Author: newboolean sunjiyan1228@163.com
+ * @Date: 2024-01-13 14:47:46
+ * @LastEditors: newboolean sunjiyan1228@163.com
+ * @LastEditTime: 2024-01-18 23:03:49
+ * @FilePath: \my-vue3-project\src\App.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
+<script setup>
+import { onLaunch, onShow, onHide } from '@dcloudio/uni-app'
+
+onLaunch(() => {
+  console.log('App Launch')
+})
+onShow(() => {
+  console.log('App Show')
+})
+onHide(() => {
+  console.log('App Hide')
+})
 </script>
 
-<style>
-/*每个页面公共css */
+<style lang="scss">
+// 字体图标
+@import '@/styles/fonts.scss';
+
+view,
+navigator,
+input,
+scroll-view {
+  box-sizing: border-box;
+}
+
+button::after {
+  border: none;
+}
+
+swiper,
+scroll-view {
+  flex: 1;
+  height: 100%;
+  overflow: hidden;
+}
+
+image {
+  width: 100%;
+  height: 100%;
+  vertical-align: middle;
+}
+
+// 两行省略
+.ellipsis {
+  overflow: hidden;
+  text-overflow: ellipsis;
+  display: -webkit-box;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+}
 </style>
+
