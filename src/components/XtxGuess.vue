71cff82d53a041125e1cbdd6b760374f27b67ac2
newboolean <sunjiyan1228@163.com>
2024-01-28 17:13:23 +0800

    feat:我的页面

--- src/components/XtxGuess.vue
+++ src/components/XtxGuess.vue
@@ -2,7 +2,7 @@
  * @Author: newboolean sunjiyan1228@163.com
  * @Date: 2024-01-14 12:34:14
  * @LastEditors: newboolean sunjiyan1228@163.com
- * @LastEditTime: 2024-01-14 23:49:19
+ * @LastEditTime: 2024-01-28 17:08:28
  * @FilePath: \my-vue3-project\src\components\XtxGuess.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -104,7 +104,7 @@ onMounted(() => {
   justify-content: space-between;
   padding: 0 20rpx;
   .guess-item {
-    width: 310rpx;
+    width: 350rpx;
     padding: 24rpx 20rpx 20rpx;
     margin-bottom: 20rpx;
     border-radius: 10rpx;
