8960f95094a8e3ccbc151f35d4b44e8e96f4484d
newboolean <sunjiyan1228@163.com>
2024-01-29 20:49:43 +0800

    feat:填写订单

--- src/services/index.js
+++ src/services/index.js
@@ -2,7 +2,7 @@
  * @Author: newboolean sunjiyan1228@163.com
  * @Date: 2024-01-13 23:13:12
  * @LastEditors: newboolean sunjiyan1228@163.com
- * @LastEditTime: 2024-01-29 19:28:35
+ * @LastEditTime: 2024-01-29 20:44:44
  * @FilePath: \my-vue3-project\src\services\index.js
  * @Description: 接口
  */
@@ -251,3 +251,133 @@ export const putMemberCartSelectedAPI = (data) => {
     data,
   });
 };
+
+/**
+ * 填写订单-获取预付订单
+ */
+ export const getMemberOrderPreAPI = () => {
+  return http({
+    method: 'GET',
+    url: '/member/order/pre',
+  })
+}
+
+/**
+ * 填写订单-获取立即购买订单
+ */
+export const getMemberOrderPreNowAPI = (data) => {
+  return http({
+    method: 'GET',
+    url: '/member/order/pre/now',
+    data,
+  })
+}
+
+/**
+ * 填写订单-再次购买
+ * @param id 订单id
+ */
+export const getMemberOrderRepurchaseByIdAPI = (id) => {
+  return http({
+    method: 'GET',
+    url: `/member/order/repurchase/${id}`,
+  })
+}
+
+/**
+ * 提交订单
+ * @param data 请求参数
+ */
+export const postMemberOrderAPI = (data) => {
+  return http({
+    method: 'POST',
+    url: '/member/order',
+    data,
+  })
+}
+
+/**
+ * 获取订单详情
+ * @param id 订单id
+ */
+export const getMemberOrderByIdAPI = (id) => {
+  return http({
+    method: 'GET',
+    url: `/member/order/${id}`,
+  })
+}
+
+/**
+ * 模拟发货-内测版
+ * @description 在DEV环境下使用，仅在订单状态为待发货时，可模拟发货，调用后订单状态修改为待收货，包含模拟物流。
+ * @param id 订单id
+ */
+export const getMemberOrderConsignmentByIdAPI = (id) => {
+  return http({
+    method: 'GET',
+    url: `/member/order/consignment/${id}`,
+  })
+}
+
+/**
+ * 确认收货
+ * @description 仅在订单状态为待收货时，可确认收货。
+ * @param id 订单id
+ */
+export const putMemberOrderReceiptByIdAPI = (id) => {
+  return http({
+    method: 'PUT',
+    url: `/member/order/${id}/receipt`,
+  })
+}
+
+/**
+ * 获取订单物流
+ * @description 仅在订单状态为待收货，待评价，已完成时，可获取物流信息。
+ * @param id 订单id
+ */
+export const getMemberOrderLogisticsByIdAPI = (id) => {
+  return http({
+    method: 'GET',
+    url: `/member/order/${id}/logistics`,
+  })
+}
+
+/**
+ * 删除订单
+ * @description 仅在订单状态为待评价，已完成，已取消时，可删除订单。
+ * @param data ids 订单集合
+ */
+export const deleteMemberOrderAPI = (data) => {
+  return http({
+    method: 'DELETE',
+    url: `/member/order`,
+    data,
+  })
+}
+
+/**
+ * 取消订单
+ * @description 仅在订单状态为待付款时，可取消订单。
+ * @param id 订单id
+ * @param data cancelReason 取消理由
+ */
+export const getMemberOrderCancelByIdAPI = (id, data) => {
+  return http({
+    method: 'PUT',
+    url: `/member/order/${id}/cancel`,
+    data,
+  })
+}
+
+/**
+ * 获取订单列表
+ * @param data orderState 订单状态
+ */
+export const getMemberOrderAPI = (data) => {
+  return http({
+    method: 'GET',
+    url: `/member/order`,
+    data,
+  })
+}
