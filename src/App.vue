85ffc264b0656eb89c737da4adb0e325ac4f2db5
newboolean <sunjiyan1228@163.com>
2024-02-01 18:27:00 +0800

    dasdasd

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
  * @Author: newboolean sunjiyan1228@163.com
  * @Date: 2024-01-13 14:47:46
  * @LastEditors: newboolean sunjiyan1228@163.com
- * @LastEditTime: 2024-01-18 23:03:49
+ * @LastEditTime: 2024-02-01 18:26:51
  * @FilePath: \my-vue3-project\src\App.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -46,6 +46,7 @@ image {
   width: 100%;
   height: 100%;
   vertical-align: middle;
+  
 }
 
 // 两行省略
