7b2d37f0a1a6daa3128a585b8bd8208551465ceb
newboolean <sunjiyan1228@163.com>
2024-02-01 17:02:33 +0800

    feat:订单列表订单详情

--- src/pages/hot/hot.vue
+++ src/pages/hot/hot.vue
@@ -183,7 +183,7 @@ page {
   justify-content: space-between;
   padding: 0 20rpx 20rpx;
   .navigator {
-    width: 315rpx;
+    width: 350rpx;
     padding: 20rpx;
     margin-top: 20rpx;
     border-radius: 10rpx;
--- src/services/index.js
+++ src/services/index.js
@@ -381,3 +381,27 @@ export const getMemberOrderAPI = (data) => {
     data,
   })
 }
+
+/**
+ * 获取微信支付参数
+ * @param data orderId 订单id
+ */
+ export const getPayWxPayMiniPayAPI = (data) => {
+  return http({
+    method: 'GET',
+    url: '/pay/wxPay/miniPay',
+    data,
+  })
+}
+
+/**
+ * 模拟支付-内测版
+ * @param data orderId 订单id
+ */
+export const getPayMockAPI = (data) => {
+  return http({
+    method: 'GET',
+    url: '/pay/mock',
+    data,
+  })
+}
